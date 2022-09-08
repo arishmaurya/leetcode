beb56e8ddfc96773695099058c1fcfa1951713d2
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-09-09 00:44:13 +0530

    Time: 4 ms (36.02%), Space: 7.6 MB (95.32%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-          int n=nums.size();
-    vector<int>dp(n,0);
-     dp[0]=nums[0];
-    int negative=0;
+        // this is tabulation way 
+    // int n=nums.size();
+    // vector<int>dp(n,0);
+    // dp[0]=nums[0];
+    // int negative=0;
+    // int i=1;
+    // for(int i=1; i<n;i++){
+    //     int take=nums[i];
+    //    if(i>1){
+    //        take+=dp[i-2];
+    //    }
+    //    int notTake= 0 + dp[i-1];
+    //    dp[i]=max(take,notTake);
+    //  } 
+    //     return dp[n-1];
+        
+        // space optimization after tabulation
+         int n=nums.size();
+    
+     int prev=nums[0];
+    int prev2=0;
     int i=1;
-   for(int i=1; i<n;i++){
-        int take=nums[i];
-       if(i>1){
-           take+=dp[i-2];
+    for(int i=1; i<n;i++){
+      int take=nums[i];
+      if(i>1){
+           take+=prev2;
        }
-       int notTake= 0 + dp[i-1];
-       dp[i]=max(take,notTake);
+       int notTake= 0 + prev;
+      int curr=max(take ,notTake);
+       prev2=prev;
+       prev=curr;
    } 
-    return dp[n-1];
+    return prev;
+        
         
     }
 };
