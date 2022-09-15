dd5a0cce608fbf429696c8da492d9fa93efb8978
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-09-16 01:51:48 +0530

    Time: 3 ms (44.90%), Space: 6.3 MB (55.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -15,22 +15,50 @@ public:
 // }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int> > dp(m,vector<int>(n,-1));
-    // return countWaysUtil(m-1,n-1,dp);
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-            if(i==0&&j==0) {
-                dp[i][j]=1;
-            continue;
+        // tabulation
+        
+    //     vector<vector<int> > dp(m,vector<int>(n,-1));
+    // // return countWaysUtil(m-1,n-1,dp);
+    //     for(int i=0; i<m; i++){
+    //         for(int j=0; j<n; j++){
+    //         if(i==0&&j==0) {
+    //             dp[i][j]=1;
+    //         continue;
+    //         }
+    //          int down=0;
+    //             int right=0;
+    //             if(i>0) down = dp[i-1][j];
+    //             if(j>0) right= dp[i][j-1];
+    //             dp[i][j]=down + right;
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+        
+        
+        // space optimaization
+         vector<int> prev(n,0);
+    for(int i=0; i<m; i++){
+        vector<int> temp(n,0);
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0){
+                temp[j]=1;
+                continue;
             }
-             int down=0;
-                int right=0;
-                if(i>0) down = dp[i-1][j];
-                if(j>0) right= dp[i][j-1];
-                dp[i][j]=down + right;
-            }
+            
+            int up=0;
+            int left =0;
+            
+            if(i>0)
+                up = prev[j];
+            if(j>0)
+                left = temp[j-1];
+                
+            temp[j] = up + left;
         }
-        return dp[m-1][n-1];
+        prev = temp;
+    }
+    
+    return prev[n-1];
         
     }
 };
