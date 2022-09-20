4166495c1f6c473035520f3fbe487ce5cbf9cdb9
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-09-20 13:06:10 +0530

    Time: 11 ms (82.27%), Space: 10.2 MB (31.13%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -31,6 +31,27 @@ public:
         return dp[n-1][m-1];
     }
     
+    int spaceoptimize(int n,int m , vector<vector<int>>&grid){
+        vector<int>prev(m,0);
+        for(int i=0; i<n; i++){
+            vector<int>curr(m,0);
+            for(int j =0; j<m; j++){
+                if(i==0&&j==0) prev[j]=grid[i][j];
+                else{
+                    int up=grid[i][j];
+                    if(i>0) up+=prev[j];
+                    else up+=1e9;
+                    int left=grid[i][j];
+                    if(j>0) left+=curr[j-1];
+                    else left+=1e9;
+                    prev[j]=min(up,left);
+                }
+            }
+            prev=curr;
+        }
+        return prev[m-1];
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
