8e1196741eb441d45f6e36d641ce5d53e507e150
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-09-23 23:11:52 +0530

    Time: 8 ms (72.96%), Space: 9.1 MB (32.49%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -16,10 +16,31 @@ vector<vector<int> > &dp) {
   
 }
 
+
+    
+int tabulation(vector<vector<int>>& triangle, int n){
+    vector<vector<int> > dp(n,vector<int>(n,0));
+    for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+    }
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+dp[i+1][j];
+            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            
+            dp[i][j] = min(down, diagonal);
+        }
+    }
+    
+    return dp[0][0];
+    }
+
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-         vector<vector<int> > dp(n,vector<int>(n,-1));
-    return minimumPathSumUtil(0,0,triangle,n,dp);
+    //      vector<vector<int> > dp(n,vector<int>(n,-1));
+    // return minimumPathSumUtil(0,0,triangle,n,dp);
+        return tabulation(triangle,n);
     }
 };
