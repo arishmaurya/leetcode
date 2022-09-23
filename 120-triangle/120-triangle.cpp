3a110eff4a8bfeaa004d363502b2159ebf008b80
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-09-23 23:13:48 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (66.58%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -37,10 +37,34 @@ int tabulation(vector<vector<int>>& triangle, int n){
     }
 
     
+    int spaceopti(vector<vector<int>>& triangle, int n){
+         vector<int> front(n,0), cur(n,0);
+    
+    for(int j=0;j<n;j++){
+        front[j] = triangle[n-1][j];
+    }
+    
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+front[j];
+            int diagonal = triangle[i][j]+front[j+1];
+            
+            cur[j] = min(down, diagonal);
+        }
+        front=cur;
+    }
+    
+    return front[0];
+    
+}
+    
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-    //      vector<vector<int> > dp(n,vector<int>(n,-1));
+    // vector<vector<int> > dp(n,vector<int>(n,-1));
     // return minimumPathSumUtil(0,0,triangle,n,dp);
-        return tabulation(triangle,n);
+    // return tabulation(triangle,n);
+    return spaceopti(triangle,n);
     }
 };
