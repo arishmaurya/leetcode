092797b036d8e5f1abb0770b342289236d9c9264
Rahul maurya <75907342+arishmaurya@users.noreply.github.com>
2022-10-05 01:15:28 +0530

    Time: 278 ms (31.48%), Space: 16.9 MB (67.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -26,10 +26,77 @@ public:
         
     }
     
+    int tab(int n,int m,vector<vector<int>>& grid ,vector<vector<vector<int>>>&dp){
+        for(int j1=0; j1<m; j1++){
+            for(int j2=0; j2<m; j2++){
+                if(j1==j2) dp[n-1][j1][j2]=grid[n-1][j1];
+                else  dp[n-1][j1][j2]=grid[n-1][j1] +grid[n-1][j2];
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j1=0;j1<m; j1++){
+                for(int j2=0; j2<m; j2++){
+                   long long  int maxi=-1e8;
+                    for(int di=-1; di<=1;di++){
+                    for(int dj=-1; dj<=1;dj++){
+                  long long  int ans;
+                    if(j1==j2) ans= grid[i][j1] ;
+                    else ans=grid[i][j1]+grid[i][j2];
+                        if(j1+di>=0&& j1+di <m&& j2+dj>=0&&j2+dj<m){
+                            ans+=dp[i + 1][j1 + di][j2 + dj];
+                        }
+                        else ans += -1e8;
+                      maxi=max(maxi,ans);  
+                    
+                }
+            }
+                    dp[i][j1][j2]=maxi;
+        }
+    }
+    }
+        return dp[0][0][m-1];
+          //Outer Nested Loops for travering DP Array
+//   for (int i = n - 2; i >= 0; i--) {
+//     for (int j1 = 0; j1 < m; j1++) {
+//       for (int j2 = 0; j2 < m; j2++) {
+
+//         int maxi = INT_MIN;
+
+//         //Inner nested loops to try out 9 options
+//         for (int di = -1; di <= 1; di++) {
+//           for (int dj = -1; dj <= 1; dj++) {
+
+//             int ans;
+
+//             if (j1 == j2)
+//               ans = grid[i][j1];
+//             else
+//               ans = grid[i][j1] + grid[i][j2];
+
+//             if ((j1 + di < 0 || j1 + di >= m) ||
+//               (j2 + dj < 0 || j2 + dj >= m))
+
+//               ans += -1e9;
+//             else
+//               ans += dp[i + 1][j1 + di][j2 + dj];
+
+//             maxi = max(ans, maxi);
+//           }
+//         }
+//         dp[i][j1][j2] = maxi;
+//       }
+//     }
+//   }
+
+//   return dp[0][0][m - 1];
+    }
+    
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return memo(0,0,m-1,n,m,grid,dp);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        // return memo(0,0,m-1,n,m,grid,dp);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        return tab(n,m,grid,dp);
     }
 };
